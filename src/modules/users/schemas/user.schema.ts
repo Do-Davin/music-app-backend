666bb4bbb008d47f513aead1570cc23da137eb5e
do-davin <dodavin96@gmail.com>
2026-03-27 22:19:05 +0700

    007-add-more-field-for-user

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,28 +1,121 @@
 import { ObjectType, Field, ID, Int } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
+// Nested: PracticeStreak
 @ObjectType()
-export class User {
+@Schema({ _id: false })
+export class PracticeStreak {
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 0 })
+  currentStreak: number;
+
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 0 })
+  longestStreak: number;
+
+  @Field({ nullable: true })
+  @Prop()
+  lastPracticeDate?: Date;
+}
+
+// Nested: PracticeGoals
+@ObjectType()
+@Schema({ _id: false })
+export class PracticeGoals {
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 30 })
+  dailyMinutes: number;
+
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 5 })
+  weeklyDays: number;
+}
+
+// Nested: Preferences
+@ObjectType()
+@Schema({ _id: false })
+export class Preferences {
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 120 })
+  defaultBpm: number;
+
+  @Field(() => Int, { nullable: true })
+  @Prop({ default: 440 })
+  tuningHz: number;
+
+  @Field({ nullable: true })
+  @Prop()
+  instrument?: string;
+}
+
+// Sub-document: RecentlyPlayed
+@ObjectType()
+@Schema({ _id: false })
+export class RecentlyPlayed {
   @Field(() => ID)
-  _id: string;
+  @Prop({ type: Types.ObjectId, ref: 'Song', required: true })
+  songId: Types.ObjectId;
 
   @Field()
+  @Prop({ required: true })
+  playedAt: Date;
+}
+
+@ObjectType()
+@Schema({ timestamps: true })
+export class User {
+  @Field(() => ID)
+  _id: Types.ObjectId;
+
+  @Field()
+  @Prop({ required: true, unique: true })
   username: string;
 
   @Field()
+  @Prop({ required: true, unique: true })
   email: string;
 
+  @Prop({ required: true })
+  password: string;
+
   @Field({ nullable: true })
+  @Prop()
   profileImageUrl?: string;
 
-  @Field(() => Int, { nullable: true })
-  practiceGoals_dailyMinutes: number;
+  @Field(() => [ID], { nullable: true })
+  @Prop({ type: [Types.ObjectId], ref: 'Song', default: [] })
+  likedSongIds: Types.ObjectId[];
 
-  @Field(() => Int, { nullable: true })
-  practiceGoals_weeklyDays: number;
+  @Field(() => [RecentlyPlayed], { nullable: true })
+  @Prop({
+    type: [RecentlyPlayed],
+    default: [],
+    validate: {
+      validator: (v: RecentlyPlayed[]) => v.length <= 20,
+      message: 'Recently played list cannot exceed 20 items',
+    },
+  })
+  recentlyPlayed: RecentlyPlayed[];
 
-  @Field(() => Int, { nullable: true })
-  practiceStreak_currentStreak: number;
+  @Field(() => PracticeGoals, { nullable: true })
+  @Prop({ type: PracticeGoals, default: () => ({}) })
+  practiceGoals: PracticeGoals;
 
-  @Field(() => Int, { nullable: true })
-  practiceStreak_longestStreak: number;
+  @Field(() => PracticeStreak, { nullable: true })
+  @Prop({ type: PracticeStreak, default: () => ({}) })
+  practiceStreak: PracticeStreak;
+
+  @Field(() => Preferences, { nullable: true })
+  @Prop({ type: Preferences, default: () => ({}) })
+  preferences: Preferences;
+
+  @Field({ nullable: true })
+  createdAt: Date;
+
+  @Field({ nullable: true })
+  updatedAt: Date;
 }
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/users/users.resolver.ts
+++ src/modules/users/users.resolver.ts
@@ -1,19 +1,35 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { User } from './schemas/user.schema';
+import { Types } from 'mongoose';
 
 @Resolver(() => User)
 export class UsersResolver {
   @Query(() => User, { name: 'me' })
   getMe(): User {
     return {
-      _id: '507f1f77bcf86cd799439011',
+      _id: new Types.ObjectId('507f1f77bcf86cd799439011'),
       username: 'davin_musician',
       email: 'davin@musicapp.com',
+      password: 'hashed_password',
       profileImageUrl: undefined,
-      practiceGoals_dailyMinutes: 30,
-      practiceGoals_weeklyDays: 5,
-      practiceStreak_currentStreak: 7,
-      practiceStreak_longestStreak: 14,
-    };
+      likedSongIds: [],
+      recentlyPlayed: [],
+      practiceGoals: {
+        dailyMinutes: 30,
+        weeklyDays: 5,
+      },
+      practiceStreak: {
+        currentStreak: 7,
+        longestStreak: 14,
+        lastPracticeDate: new Date('2026-03-26'),
+      },
+      preferences: {
+        defaultBpm: 120,
+        tuningHz: 440,
+        instrument: 'guitar',
+      },
+      createdAt: new Date('2026-01-01'),
+      updatedAt: new Date('2026-03-27'),
+    } as User;
   }
 }
