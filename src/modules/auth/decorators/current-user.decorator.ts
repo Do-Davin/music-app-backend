eb9330d1112de2849c1b24b652c7f6b91b9602da
do-davin <dodavin96@gmail.com>
2026-03-28 09:59:05 +0700

    008-impl-login-impl-fetch-current-user-and-jwt-auth-guard

---
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,17 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export interface CurrentUserData {
+  userId: string;
+  email: string;
+  username: string;
+}
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof CurrentUserData | undefined, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext<{ req: { user: CurrentUserData } }>().req.user;
+
+    return data ? user?.[data] : user;
+  },
+);
